f689725bcfae611e69c50cc42215d78247404162
RuizSP <84715726+RuizSP@users.noreply.github.com>
2023-05-04 10:11:37 -0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import Pyro4
 
-Stream = Pyro4.Proxy("PYRONAME:servidor.Fala")
+Stream = Pyro4.Proxy("PYRONAME:servidor.Fala@172.25.5.216:9090")
 
 def main():
     texto = input("Digite seu texto aqui: ")
-    Stream.textReceiver(texto)
+    language = input("Digite pt-br")
+    Stream.textReceiver(texto,language)
 
 
 if __name__ == "__main__":
--- Servidor.py
+++ Servidor.py
@@ -5,17 +5,17 @@ from gtts import gTTS
 @Pyro4.expose
 
 class StreamServer (object):
-    def textReceiver(self, texto):
-        tts = gTTS(texto, lang = 'pt-br')
+    def textReceiver(self, texto, language):
+        tts = gTTS(texto, lang = language)
         tts.save("audioInfo.mp3")
         os.system("mpg321 audioInfo.mp3")
-        os.system('mplayer audioInfo.mp3')
+        #os.system('mplayer audioInfo.mp3')
 
 
 def main():
     Pyro4.Daemon.serveSimple({
         StreamServer: "servidor.Fala"
-    })
+    },  host = '172.25.5.216',)
 if __name__ == "__main__":
     main()
 
